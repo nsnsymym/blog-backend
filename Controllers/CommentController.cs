8ec215d092a555f29d0f98b9c4d249cbe1c7b2ef
nsnsymym <mickey-ya@outlook.jp>
2021-04-29 19:29:06 +0900

    update: namespace変更

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace blog_backend.Controllers
+namespace BlogBackend.Controllers
 {
     [Route("api/[controller]")]
     public class CommentController : Controller
