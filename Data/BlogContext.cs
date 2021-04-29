468d7a8e6d45686a226f363df95f8d832cc5cc4c
nsnsymym <mickey-ya@outlook.jp>
2021-04-29 19:29:17 +0900

    add: DB接続

---
+++ Data/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BlogBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogBackend.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticleTag> ArticleTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
