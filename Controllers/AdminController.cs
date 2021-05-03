2271651a0861e720f068982a7c90a92dd2c008af
nsnsymym <mickey-ya@outlook.jp>
2021-05-03 23:18:33 +0900

    update: AdminControllerにCRUD追加

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,46 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BlogBackend.Models;
+using System.Net.Mime;
+using BlogBackend.Utils;
+using BlogBackend.Data.Requests;
+using System.Threading.Tasks;
+using BlogBackend.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BlogBackend.Controllers
 {
+    [Produces(MediaTypeNames.Application.Json)]
     [Route("api/[controller]")]
     public class AdminController : Controller
     {
-        // GET: api/values
+        private readonly BlogContext _context;
+
+        public AdminController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Admins
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<Admin>>> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Admins.ToListAsync();
         }
 
-        // GET api/values/5
+        // GET api/Admins/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Admin>> Get(int id)
         {
-            return "value";
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return admin;
         }
 
-        // POST api/values
+        // POST api/Admins
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("api/admin")]
+        public async Task<ActionResult<Admin>> Post([FromBody] Admin admin)
         {
+            _context.Admins.Add(admin);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAdmin", new { id = admin.Id }, admin);
         }
 
-        // PUT api/values/5
+        // PUT api/Admins/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Admin admin)
         {
+            if (id != admin.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(admin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdminExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/values/5
+        // DELETE api/Admins/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<Admin>> Delete(int id)
         {
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            _context.Admins.Remove(admin);
+            await _context.SaveChangesAsync();
+
+            return admin;
+
+        }
+
+        private bool AdminExists(int id)
+        {
+            return _context.Admins.Any(e => e.Id == id);
         }
     }
 }
