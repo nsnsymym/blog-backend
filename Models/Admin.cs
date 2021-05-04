7120c7b32f72a720be055155074eba89caf7b3a2
nsnsymym <mickey-ya@outlook.jp>
2021-05-04 09:11:28 +0900

    update: Modelのアノテーション

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BlogBackend.Models
@@ -10,14 +11,19 @@ namespace BlogBackend.Models
         }
 
         public int Id { get; set; }
+        [Display(Name = "ユーザー名")]
         [Required]
-        [StringLength(50, ErrorMessage = "ユーザー名は50文字までです。")]
+        [StringLength(50, ErrorMessage = "{0}は{1}文字までです。")]
         public string UserName { get; set; }
+        [Display(Name = "パスワード")]
         [Required]
-        [MinLength(6, ErrorMessage = "パスワードは6文字以上必要です。")]
+        [MinLength(6, ErrorMessage = "{0}は{1}文字以上必要です。")]
         public string Password { get; set; }
+        [Display(Name = "アイコン画像")]
         public string Icon { get; set; }
+        [Display(Name = "TwitterID")]
         public string Twitter { get; set; }
+        [Display(Name = "GithubID")]
         public string Github { get; set; }
     }
 }
