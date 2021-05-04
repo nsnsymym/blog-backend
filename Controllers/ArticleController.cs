be1f6a4e568f7432ddc9f5534ccbffd580bc2c2f
nsnsymym <mickey-ya@outlook.jp>
2021-05-05 03:27:44 +0900

    add: ArticleController作成

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -1,46 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using BlogBackend.Data;
+using BlogBackend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BlogBackend.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     public class ArticleController : Controller
     {
+        private readonly BlogContext _context;
+
+        public ArticleController(BlogContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<Article>>> GetAllAsync()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Articles.ToListAsync();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Article>> GetAsync(Guid id)
         {
-            return "value";
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return article;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Article>> PostAsync([Bind("Id,Title,Contents,Status,Like")] Article article)
         {
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAsync), new { id = article.Id }, article);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<Article>> PutAsync(Guid id, [Bind("Id,Title,Contents,Status,Like")] Article article)
         {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<Article>> DeleteAsync(Guid id)
         {
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return article;
+
+        }
+
+        private bool ArticleExists(Guid id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
         }
     }
 }
